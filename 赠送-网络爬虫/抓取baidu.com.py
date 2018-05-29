02561e979e7bbf88d46b6d2916cdb33780c23663
胡小易 <microease@163.com>
2018-05-29 19:39:04 +0800

    add

--- 赠送-网络爬虫/抓取baidu.com.py
+++ 赠送-网络爬虫/抓取baidu.com.py
@@ -1,10 +1,12 @@
 #coding=utf-8
 import requests
+#抓取网页
 reponse = requests.get("http://www.baidu.com")
 print(reponse.text)
 print(reponse.headers)
 print(reponse.status_code)
+#保存图片
 image = requests.get("http://www.wzxdm.com/wp-content/uploads/2018/05/path.jpg")
 with open("huyankai.png","wb") as f:
     f.write(image.content)
-    f.close()
+    f.close() 
---
+++ 赠送-网络爬虫/调用浏览器.py
@@ -0,0 +1,4 @@
+#coding=utf-8
+from selenium import webdriver
+driver = webdriver.Chrome(r"/Users/huxiaoyi/desktop/chromedriver")
+driver.get("http://www.baidu.com")

b6ab171d3b81677c00a164a6a345e9f478117389
胡小易 <microease@163.com>
2018-06-28 22:01:18 +0800

    add

--- 002个人项目/00002www.mylikesz.com/1.py
+++ 002个人项目/00002www.mylikesz.com/1.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 r = requests.get('http://www.mylikesz.com/')
 r.encoding='utf-8'
-print(r.text)
+soup = BeautifulSoup(r.text, 'html.parser')
+
 getContent = soup.find('div',class='nav').find('div',class='Con').find('div',class='nav_a')
 print(getContent)

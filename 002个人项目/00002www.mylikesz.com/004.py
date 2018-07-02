6584d172447bebd03049e5d47a77b9c4b5be9b78
浮生若梦 <microease@163.com>
2018-07-02 22:53:32 +0800

    add

--- 002个人项目/00002www.mylikesz.com/004.py
+++ 002个人项目/00002www.mylikesz.com/004.py
@@ -1,17 +1,39 @@
 # coding=utf-8
 import requests
 from bs4 import BeautifulSoup
+import os
 
 # 文章很多，此处仅仅放上五篇测试
 article = ['http://www.mylikesz.com/html/78935.html', 'http://www.mylikesz.com/html/78937.html',
            'http://www.mylikesz.com/html/78952.html', 'http://www.mylikesz.com/html/78957.html',
            'http://www.mylikesz.com/html/78959.html']
+
+
+def txt(name, txt):
+    b = os.getcwd() + '\\articles\\'
+    if not os.path.exists(b):
+        os.makedirs(b)
+        mktxt = b + name + '.txt'
+    else:
+        return 0
+    file = open(mktxt, 'w')
+    file.write(txt)
+    file.close()
+    print('ok')
+titles=[]
+contents= []
 for i in article:
     r = requests.get(i)
     r.encoding = 'utf-8'
     soup = BeautifulSoup(r.text, 'html.parser')
-    titles = soup.find_all('h1')
-    times = soup.find_all('div',{"class": "essay_xb_info"})
-    for j in times:
-        for k in j.find_all('span'):
-            print(type(k))
+    titles.append(soup.find('h1').get_text())
+    contents.append(soup.find('div', {"class": "essay_xb_info"}))
+    # for j in times:
+    #     for k in j.find_all('span'):
+    #         print(k)
+    contents = soup.find('div', {"class": "text_C"})
+    # txt(str(titles), str(contents))
+
+for l in titles:
+    index = titles.index(l)
+    txt(str(titles[index]), '111')

380f352d5de0e1a9855147481b2c27df38a98c0c
浮生若梦 <microease@163.com>
2018-07-05 12:09:35 +0800

    add

--- 002个人项目/00002www.mylikesz.com/004.py
+++ 002个人项目/00002www.mylikesz.com/004.py
@@ -6,14 +6,11 @@ import sys
 
 # 文章很多，此处仅仅放上五篇测试
 article = ['http://www.mylikesz.com/html/1052.html', 'http://www.mylikesz.com/html/1113.html', 'http://www.mylikesz.com/html/18511.html', 'http://www.mylikesz.com/html/23848.html', 'http://www.mylikesz.com/html/24004.html']
-
-
-
 def txt(name, txt):
-    b = os.getcwd() + '/articles/'
+    b = os.getcwd() + '\\articles\\'
     print(b)
-    if not os.path.exists(b):
-        os.makedirs(b)
+    # if not os.path.exists(b):
+    #     os.makedirs(b)
     mktxt = b + name + '.txt'
     file = open(mktxt, 'wb+')
     file.write(txt)
@@ -29,25 +26,22 @@ for i in article:
     r.encoding = 'utf-8'
     soup = BeautifulSoup(r.text, 'html.parser')
     titles.append(soup.find('h1').get_text())
-<<<<<<< HEAD
     # contents.append(soup.find('div', {"class": "essay_xb_info"}))
     # for j in times:
     #     for k in j.find_all('span'):
     #         print(k)
     contents.append(soup.find('div', {"class": "text_C"}))
     # txt(str(titles), str(contents))
-
-for l in range(len(titles)):
-    txt(str(titles[l]), str(contents[l]))
+#
+# for l in range(len(titles)):
+#     txt(str(titles[l]), str(contents[l]))
 #
 # for l in titles:
 #     index = titles.index(l)
 #     txt(str(titles[index]), str(contents[index]))
-=======
-    contents.append(soup.find('div', {"class": "text_C"}))
+#     contents.append(soup.find('div', {"class": "text_C"}))
     times.append(soup.find('div', {"class": "essay_xb_info"}).find('span').get_text())
 # 保存到txt
 for l in titles:
     index = titles.index(l)
     txt(str(times[index] + titles[index]), str(contents[index]).encode('utf-8'))
->>>>>>> bef676ac716ed82dfb8275d9081fcf14b14ea097

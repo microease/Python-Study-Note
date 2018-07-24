d1a0175a2b0a0d979c0c5ca599c801830479c8d7
浮生若梦 <microease@163.com>
2018-07-24 10:06:22 +0800

    add

--- 003刷题/001菜鸟教程_Python100例/006.py
+++ 003刷题/001菜鸟教程_Python100例/006.py
@@ -1,9 +1,2 @@
 # 题目：斐波那契数列。
-def Fibonacci_Yield_tool(n):
-    a, b = 0, 1
-    while n > 0:
-        yield b
-        a, b = b, a + b
-        n -= 1
-def Fibonacci_Yield(n):
-    return list(Fibonacci_Yield_tool(n))
+def Fib(n):

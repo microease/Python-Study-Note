02a666516c2820651bf8138dcffdd8028912ddd7
浮生若梦 <microease@163.com>
2018-07-27 11:02:35 +0800

    add

--- 005Books/006_Book_Python3网络爬虫开发实战/003基本库的使用/00002.py
+++
@@ -1,6 +0,0 @@
-import urllib.parse
-import urllib.request
-
-data1 = bytes(urllib.parse.urlencode({'word':'hello huyankai'}),encoding='utf-8')
-response = urllib.request.urlopen('http://httpbin.org/post',data = data1)
-print(response.read())
--- 005Books/006_Book_Python3网络爬虫开发实战/003基本库的使用/00003.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-r = requests.get('https://github.com/favicon.ico')
-f = open('favicon.ico', 'wb')
-f.write(r.content)

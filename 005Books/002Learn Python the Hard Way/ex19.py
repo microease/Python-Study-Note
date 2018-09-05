3e48f3c4cdd1ab76fe9dd0c10b23903796a2dcf4
microease <microease@163.com>
2018-09-05 19:02:44 +0800

    add

---
+++ 005Books/002Learn Python the Hard Way/ex19.py
@@ -0,0 +1,5 @@
+def cheese_and_crckers(cheese_count, boxes_of_crackers):
+    print("You have %d cheeses!" % cheese_count)
+    print("You have %d boxes of crackers!" % boxes_of_crackers)
+    print("Man that's enough for a party!")
+    print("Get a blanket.\n")

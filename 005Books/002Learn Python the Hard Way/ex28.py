5f19070fff7b9c56ec2ebab45fd039abc4de2370
microease <microease@163.com>
2018-09-07 07:44:12 +0800

    add

---
+++ 005Books/002Learn Python the Hard Way/ex28.py
@@ -0,0 +1,40 @@
+ex1 = True and True
+ex2 = False and True
+ex3 = 1 == 1 and 2 == 1
+ex4 = "test" == "test"
+ex5 = 1 == 1 or 2 != 1
+ex6 = True and 1 == 1
+ex7 = False and 0 != 0
+ex8 = True or 1 == 1
+ex9 = "test" == "testing"
+ex10 = 1 != 0 and 2 == 1
+ex11 = "test" != "testing"
+ex12 = "test" == 1
+ex13 = not (True and False)
+ex14 = not (1 == 1 and 0 != 1)
+ex15 = not (10 == 1 or 1000 == 1000)
+ex16 = not (1 != 10 or 3 == 4)
+ex17 = not ("testing" == "testing" and "Zed" == "Cool Guy")
+ex18 = 1 == 1 and not ("testing" == 1 or 1 == 0)
+ex19 = "chunky" == "bacon" and not (3 == 4 or 3 == 3)
+ex20 = 3 == 3 and not ("testing" == "testing" or "Python" == "Fun")
+print(ex1)
+print(ex2)
+print(ex3)
+print(ex4)
+print(ex5)
+print(ex6)
+print(ex7)
+print(ex8)
+print(ex9)
+print(ex10)
+print(ex11)
+print(ex12)
+print(ex13)
+print(ex14)
+print(ex15)
+print(ex16)
+print(ex17)
+print(ex18)
+print(ex19)
+print(ex20)

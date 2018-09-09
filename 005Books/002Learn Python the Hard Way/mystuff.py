c04c21abfe6ce34449b9cd96c9cfd8e49c0f2f12
microease <microease@163.com>
2018-09-09 17:50:50 +0800

    add

---
+++ 005Books/002Learn Python the Hard Way/mystuff.py
@@ -0,0 +1,12 @@
+# this goes in my stuff
+def apple():
+    print("I am Apples")
+
+
+class MyStuff(object):
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+    def apple(self):
+        print("I AM CLASSY APPLES!")
+
+print(MyStuff().tangerine)

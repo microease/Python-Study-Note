2f81714cc1bc9bc04f9cefc21448385d24f78b0e
microease <microease@163.com>
2018-09-27 05:50:10 +0800

    add

--- 001黑马/2018/00Python基础/22-名片管理系统/cards_main.py
+++ 001黑马/2018/00Python基础/22-名片管理系统/cards_main.py
@@ -1,18 +1,21 @@
 import cards_input
 import cards_tools
 
-print("*" * 30)
-print("欢迎使用【名片管理系统】V1.0")
-print("1：新建名片")
-print("2：显示全部")
-print("3：查询名片")
-print("0：退出系统")
-print("*" * 30)
-action_str = input("请选择需要执行的操作:")
-print("您选择的操作是『%s』" % action_str)
-if action_str in ["1","2","3"]:
-    pass
-elif action_str =="0":
-    pass
-else:
-    print("您输入的条件不正确，请重新选择")
+# TODO 显示功能菜单
+while True:
+    cards_tools.show_menus()
+
+    action_str = input("请选择需要执行的操作:")
+    print("您选择的操作是『%s』" % action_str)
+    if action_str in ["1", "2", "3"]:
+        if action_str == "1":
+            cards_tools.new_card()
+        elif action_str == "2":
+            cards_tools.show_all()
+        elif action_str == "3":
+            cards_tools.search_card()
+    elif action_str == "0":
+        print("欢迎再次使用名片管理系统")
+        break
+    else:
+        print("您输入的条件不正确，请重新选择")
---
+++ 001黑马/2018/00Python基础/22-名片管理系统/cards_tools.py
@@ -0,0 +1,27 @@
+def show_menus():
+    """显示菜单"""
+    print("*" * 30)
+    print("欢迎使用【名片管理系统】V1.0")
+    print("1：新建名片")
+    print("2：显示全部")
+    print("3：查询名片")
+    print("0：退出系统")
+    print("*" * 30)
+
+def new_card():
+    """新增名片"""
+    print("-"*30)
+    print("新增名片")
+
+def show_all():
+    """显示所有名片"""
+    print("-"*30)
+    print("显示所有名片")
+
+
+def search_card():
+    """搜索名片"""
+    print("-"*30)
+    print("搜索名片")
+
+

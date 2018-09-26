25cb769d1fb31e25fb148f7c4c88078610a39c69
microease <microease@163.com>
2018-09-26 20:08:57 +0800

    add

---
+++ 001黑马/2018/00Python基础/19-字典/01-第一个字典.py
@@ -0,0 +1,17 @@
+xiaoming = {
+    "name": "小明",
+    "age": 18,
+    "gender": True,
+    "height": 1.75
+}
+print(xiaoming["name"])
+
+xiaoming.update({"xx":"11"})
+print(xiaoming)
+
+xiaoming.pop("xx")
+
+print(xiaoming)
+
+xiaoming.clear()
+print(xiaoming)

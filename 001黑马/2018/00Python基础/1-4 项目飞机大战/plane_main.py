b0ce9d1e28c3d70276b8b1ec7e4d532f66a31d8a
microease <microease@163.com>
2018-10-15 00:23:21 +0800

    add

--- 001黑马/2018/00Python基础/1-4 项目飞机大战/plane_main.py
+++ 001黑马/2018/00Python基础/1-4 项目飞机大战/plane_main.py
@@ -9,9 +9,13 @@ class PlaneGame(object):
         print("游戏初始化")
         self.screen = pygame.display.set_mode(SCREEN_RECT.size)
         self.clock = pygame.time.Clock()
+        self.__create_sprites()
 
     def __create_sprites(self):
-        pass
+        bg1 = Background()
+        bg2 = Background(True)
+        bg2.rect.y = -bg2.rect.height
+        self.back_group = pygame.sprite.Group(bg1,bg2)
 
     def start_game(self):
         print("游戏开始")
@@ -21,7 +25,36 @@ class PlaneGame(object):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-            pass
+            # 设置刷新帧率
+            self.clock.tick(FRAME_PER_SEC)
+            # 事件监听
+            self.__event_handler()
+            # 碰撞检测
+            self.__check_collide()
+            # 更新、绘制精灵组
+            self.__update_sprites()
+            # 更新显示
+            pygame.display.update()
+        pass
+
+    def __event_handler(self):
+        for event in pygame.event.get():
+            # 判断是否退出游戏
+            if event.type == pygame.QUIT:
+                PlaneGame.__game_over()
+
+    def __check_collide(self):
+        pass
+
+    def __update_sprites(self):
+        self.back_group.update()
+        self.back_group.draw(self.screen)
+
+    @staticmethod
+    def __game_over():
+        print("游戏结束")
+        pygame.quit()
+        exit()
 
 
 if __name__ == '__main__':
--- 001黑马/2018/00Python基础/1-4 项目飞机大战/plane_sprites.py
+++ 001黑马/2018/00Python基础/1-4 项目飞机大战/plane_sprites.py
@@ -1,14 +1,31 @@
 import pygame
 
-SCREEN_RECT = pygame.Rect(0,0,480,700)
+# 屏幕大小的常量
+SCREEN_RECT = pygame.Rect(0, 0, 480, 700)
+# 刷新帧率
+FRAME_PER_SEC = 60
 
 
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, image_name, speed=1):
-        super.__init__()
+        super().__init__()
         self.image = pygame.image.load(image_name)
         self.rect = self.image.get_rect()
         self.speed = speed
 
     def update(self):
+        # 在屏幕的垂直方向上移动
         self.rect.y += self.speed
+
+
+class Background(GameSprite):
+    """游戏背景精灵"""
+
+    def __init__(self, is_alt=False):
+        super().__init__("./images/background.png")
+        if is_alt:
+            self.rect.y = -self.rect.height
+    def update(self):
+        super().update()
+        if self.rect.y >= SCREEN_RECT.height:
+            self.rect.y == -SCREEN_RECT.height
